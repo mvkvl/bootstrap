99d6ab9400d576212138ef179933e86007d43c20
Mikhail Kantur <kami@slink.ws>
2013-10-04 09:26:25 +1100

    ...

--- bootstrap_min.sh
+++ bootstrap_min.sh
@@ -9,4 +9,7 @@ echo "*********************************"
 echo
 
 sleep 3
+sudo apt-get -y dist-upgrade
+sudo apt-get update
+sudo apt-get -y upgrade
 curl https://raw.github.com/mvkvl/bootstrap/master/setup_common.sh | bash
