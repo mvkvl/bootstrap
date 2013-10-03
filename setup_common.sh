f30aad26a9918fd54f2459c271dcf6107488f94a
Mikhail Kantur <kami@slink.ws>
2013-10-03 22:20:19 +1100

    ...

--- setup_common.sh
+++ setup_common.sh
@@ -7,10 +7,10 @@ git clone https://github.com/mvkvl/bootstrap.git
 
 # add 1Gb swap file
 # http://www.rownet.co.uk/adding-swap-space-to-an-amazon-ec2-micro-instance/
-#sudo dd if=/dev/zero of=/swapfile bs=1M count=1024
-#sudo mkswap /swapfile
-#sudo swapon /swapfile
-#sudo echo "/swapfile  swap      swap    defaults         0 0" > /etc/fstab
+sudo dd if=/dev/zero of=/swapfile bs=1M count=1024
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo echo "/swapfile  swap      swap    defaults         0 0" > /etc/fstab
 
 # secure an instance (if username is set)
 if [ $EC_USERNAME != "ubuntu" ]; then
@@ -27,9 +27,11 @@ else
 fi
 
 # Install neded packages
-# sudo apt-get -y install htop mc
+sudo apt-get -y install htop mc
 
 # https://launchpad.net/~cassou/+archive/emacs
-# sudo apt-add-repository -y ppa:cassou/emacs
-# sudo apt-get update
-# sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
+sudo apt-add-repository -y ppa:cassou/emacs
+sudo apt-get update
+sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
+
+# sudo apt-get -y upgrade
