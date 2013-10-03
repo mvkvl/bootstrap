c1abf0d379bec6e8611b045c849545f0b090978e
Mikhail Kantur <kami@slink.ws>
2013-10-04 10:18:21 +1100

    ...

--- setup_web.sh
+++ setup_web.sh
@@ -4,15 +4,16 @@
 
 # install apache'n'friends
 sudo apt-get -y install apache2 php5 libapache2-mod-php5
-
-# mysql-server mysql-client php5 php5-mysql php5-curl php5-gd php-apc
-
 sudo a2enmod rewrite
 sudo a2enmod expires
 sudo a2enmod headers
 sudo a2enmod deflate
 sudo mkdir -p /var/www
 
+# install mysql
+echo "mysql-server mysql-server/root_password password 123" | debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password 123" | debconf-set-selections
+sudo apt-get -y install mysql-server
 
 # install wordpress
 
