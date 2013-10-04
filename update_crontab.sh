d2f53979be22d4b7964ca76cedc57285a45a2810
Mikhail Kantur <kami@slink.ws>
2013-10-04 19:08:01 +1100

    ...

--- update_crontab.sh
+++ update_crontab.sh
@@ -11,10 +11,10 @@ echo $HOME
   chmod +x $HOME/dns-update.sh
   cp /etc/crontab /tmp
   sudo chmod 666 /tmp/crontab
-  chattr -i /tmp/crontab
-  echo ""  >> /tmp/crontab
-  echo "# update FreeDNS every 5 minutes" >> /tmp/crontab
-  echo "*/5 * * * * $USER $HOME/dns-update.sh" >> /tmp/crontab
+  sudo chattr -i /tmp/crontab
+  sudo echo ""  >> /tmp/crontab
+  sudo echo "# update FreeDNS every 5 minutes" >> /tmp/crontab
+  sudo echo "*/5 * * * * $USER $HOME/dns-update.sh" >> /tmp/crontab
   sudo chmod 644 /tmp/crontab
   chattr +i /tmp/crontab
   sudo mv /tmp/crontab /etc
