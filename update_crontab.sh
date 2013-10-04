6281edb55920dfc8971139dadd435577c1dab24e
Mikhail Kantur <kami@slink.ws>
2013-10-04 20:28:31 +1100

    ...

--- update_crontab.sh
+++ update_crontab.sh
@@ -7,21 +7,8 @@ echo $HOME
 #echo
 
 # update crontab for FreeDNS
-
-# sudo chattr -i /tmp/dns-update
-
   set +o noclobber
-  #sudo touch /tmp/dns-update
   sudo echo "#!/bin/bash"  > /tmp/dns-update  
   sudo echo "$DYNDNS_UPDATE_CMD" >> /tmp/dns-update  
   sudo chmod +x /tmp/dns-update
   sudo cp /tmp/dns-update /etc/cron.hourly/
-
-#  cp /etc/crontab /tmp
-#  sudo chmod 666 /tmp/crontab
-#  sudo chattr -i /tmp/crontab
-#  sudo echo ""  >> /tmp/crontab
-#  sudo echo "# update FreeDNS every 5 minutes" >> /tmp/crontab
-#  sudo echo "*/5 * * * * $USER $HOME/dns-update.sh" >> /tmp/crontab
-#  sudo chmod 644 /tmp/crontab
-#  sudo mv /tmp/crontab /etc
