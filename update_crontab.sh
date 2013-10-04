3241fe5f9541cfc205cdd260c159a45bc1fee852
Mikhail Kantur <kami@slink.ws>
2013-10-04 22:50:12 +1100

    ...

--- update_crontab.sh
+++ update_crontab.sh
@@ -12,9 +12,11 @@ echo $HOME
   sudo mv /tmp/dns-update $HOME
 
   sudo cp /etc/crontab /tmp
+  sudo chattr -i /tmp/crontab
+  sudo chattr -i /etc
   sudo echo "" >> /tmp/crontab
   sudo echo "# update FreeDNS every 5 minutes" >> /tmp/crontab
-  sudo echo "*/5 *   * * *   kami    /home/kami/work/scripts/freedns.update" >> /tmp/crontab
+  sudo echo "*/5 * * * * ubuntu $HOME/dns-update" >> /tmp/crontab
   sudo mv /tmp/crontab /etc/
 
   echo "----- crontab ------"
