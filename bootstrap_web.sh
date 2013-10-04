694e29e4bbd6bb018549f895288c67db917f65eb
Mikhail Kantur <kami@slink.ws>
2013-10-04 22:03:16 +1100

    ...

--- bootstrap_web.sh
+++ bootstrap_web.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 echo
 echo "*********************************"
 echo "*                               *"
@@ -9,8 +8,7 @@ echo "*                               *"
 echo "*********************************"
 echo
 
-
-echo "DYN DNS CMD = $DYNDNS_UPDATE_CMD"
+# echo "DYN DNS CMD = $DYNDNS_UPDATE_CMD"
 
 export DEBIAN_FRONTEND=noninteractive
 
@@ -18,13 +16,12 @@ sleep 3
 sudo apt-get -y dist-upgrade
 sudo apt-get update
 sudo apt-get -y upgrade
-# curl https://raw.github.com/mvkvl/bootstrap/master/setup_common.sh | bash
+curl https://raw.github.com/mvkvl/bootstrap/master/setup_common.sh | bash
 curl https://raw.github.com/mvkvl/bootstrap/master/setup_web.sh | bash
 sudo apt-get -y autoremove
 
-# WP_DB_PASS=`cat /tmp/wp.db.pass`
-# sudo rm /tmp/wp.db.pass
-
+WP_DB_PASS=`cat /tmp/wp.db.pass`
+sudo rm /tmp/wp.db.pass
 echo
 echo "Wordpress credentials"
 echo "    db: wpdb"
--- setup_web.sh
+++ setup_web.sh
@@ -31,48 +31,48 @@ sudo mkdir -p /var/www
 
 # install mysql
 # http://gercogandia.blogspot.ru/2012/11/automatic-unattended-install-of.html
-# sudo echo "mysql-server mysql-server/root_password password $MYSQL_ADMIN_PASSWORD" | sudo debconf-set-selections
-# sudo echo "mysql-server mysql-server/root_password_again password $MYSQL_ADMIN_PASSWORD" | sudo debconf-set-selections
-# sudo apt-get -y install mysql-server mysql-client php5-mysql libapache2-mod-auth-mysql
+sudo echo "mysql-server mysql-server/root_password password $MYSQL_ADMIN_PASSWORD" | sudo debconf-set-selections
+sudo echo "mysql-server mysql-server/root_password_again password $MYSQL_ADMIN_PASSWORD" | sudo debconf-set-selections
+sudo apt-get -y install mysql-server mysql-client php5-mysql libapache2-mod-auth-mysql
 
 # create database
 #-mysqladmin --user=root --password=$MYSQL_ADMIN_PASSWORD create wordpress
 
 # create user & database
-# WP_DB_PASS=`cat /dev/urandom | tr -dc A-Za-z0-9 | head -c 10`
-# echo  $WP_DB_PASS > /tmp/wp.db.pass
-# echo "create database wpdb;" > /tmp/database.sql
-# echo "connect wpdb;" >> /tmp/database.sql
-# echo "create user 'wordpress'@'localhost' identified by '$WP_DB_PASS';" >> /tmp/database.sql
-# echo "grant all privileges on wpdb.* to 'wordpress'@'localhost' identified by '$WP_DB_PASS';" >> /tmp/database.sql
-# echo "flush privileges;" >> /tmp/database.sql
-# mysql --user=root --password=$MYSQL_ADMIN_PASSWORD < /tmp/database.sql
+WP_DB_PASS=`cat /dev/urandom | tr -dc A-Za-z0-9 | head -c 10`
+echo  $WP_DB_PASS > /tmp/wp.db.pass
+echo "create database wpdb;" > /tmp/database.sql
+echo "connect wpdb;" >> /tmp/database.sql
+echo "create user 'wordpress'@'localhost' identified by '$WP_DB_PASS';" >> /tmp/database.sql
+echo "grant all privileges on wpdb.* to 'wordpress'@'localhost' identified by '$WP_DB_PASS';" >> /tmp/database.sql
+echo "flush privileges;" >> /tmp/database.sql
+mysql --user=root --password=$MYSQL_ADMIN_PASSWORD < /tmp/database.sql
 
 # install wordpress
-# cd /tmp
-# wget http://wordpress.org/latest.tar.gz
-# sleep 10
-# tar xfvz latest.tar.gz
-# sudo mv wordpress /var/www
+cd /tmp
+wget http://wordpress.org/latest.tar.gz
+sleep 10
+tar xfvz latest.tar.gz
+sudo mv wordpress /var/www
 
-# echo "<VirtualHost *:80>"                 > /tmp/wordpress.apache
-# echo "  ServerName $HOSTNAME"            >> /tmp/wordpress.apache
-# echo "  DocumentRoot /var/www/wordpress" >> /tmp/wordpress.apache
-# echo "  DirectoryIndex index.php"        >> /tmp/wordpress.apache
-# echo "  <Directory /var/www/wordpress/>" >> /tmp/wordpress.apache
-# echo "    AllowOverride All"             >> /tmp/wordpress.apache
-# echo "    Order Deny,Allow"              >> /tmp/wordpress.apache
-# echo "    Allow from all"                >> /tmp/wordpress.apache
-# echo "  </Directory>"                    >> /tmp/wordpress.apache
-# echo "</VirtualHost>"                    >> /tmp/wordpress.apache
+echo "<VirtualHost *:80>"                 > /tmp/wordpress.apache
+echo "  ServerName $HOSTNAME"            >> /tmp/wordpress.apache
+echo "  DocumentRoot /var/www/wordpress" >> /tmp/wordpress.apache
+echo "  DirectoryIndex index.php"        >> /tmp/wordpress.apache
+echo "  <Directory /var/www/wordpress/>" >> /tmp/wordpress.apache
+echo "    AllowOverride All"             >> /tmp/wordpress.apache
+echo "    Order Deny,Allow"              >> /tmp/wordpress.apache
+echo "    Allow from all"                >> /tmp/wordpress.apache
+echo "  </Directory>"                    >> /tmp/wordpress.apache
+echo "</VirtualHost>"                    >> /tmp/wordpress.apache
 
-# sudo mv /tmp/wordpress.apache /etc/apache2/sites-available/wordpress
-# sudo a2ensite wordpress
-# sudo service apache2 restart
+sudo mv /tmp/wordpress.apache /etc/apache2/sites-available/wordpress
+sudo a2ensite wordpress
+sudo service apache2 restart
 
 # configure wordpress
-# cd /var/www
-# sudo chmod -R 777 wordpress
+cd /var/www
+sudo chmod -R 777 wordpress
 
 # create script for change permissions
 #echo "sudo find /var/www/wordpress -type d -exec chmod 755 {} \;"  > /tmp/change.permissions.sh
@@ -80,10 +80,10 @@ sudo mkdir -p /var/www
 #sudo mv /tmp/change.permissions.sh 
 
 # make test files
-# echo "<?php phpinfo()?>" > /tmp/test.php
-# echo "<html><body><h2>It seems to be working!</h2></body></html>" > /tmp/test.html
-# sudo mv /tmp/test.php  /var/www
-# sudo mv /tmp/test.html /var/www
+echo "<?php phpinfo()?>" > /tmp/test.php
+echo "<html><body><h2>It seems to be working!</h2></body></html>" > /tmp/test.html
+sudo mv /tmp/test.php  /var/www
+sudo mv /tmp/test.html /var/www
 
 # update crontab
 echo "----- check for update -----"
@@ -95,4 +95,4 @@ if [ -n "$DYNDNS_UPDATE_CMD" ]; then
 fi
 
 # chown
-# sudo chown -R www-data:www-data /var/www
+sudo chown -R www-data:www-data /var/www
