04894cab209789ffb28732428e1101b339f416d6
Mikhail Kantur <kami@slink.ws>
2013-10-04 11:43:33 +1100

    ...

--- bootstrap_web.sh
+++ bootstrap_web.sh
@@ -17,4 +17,12 @@ sudo apt-get -q -y upgrade
 curl https://raw.github.com/mvkvl/bootstrap/master/setup_common.sh | bash
 curl https://raw.github.com/mvkvl/bootstrap/master/setup_web.sh | bash
 sudo apt-get -q -y autoremove
+
+echo
+echo "Wordpress credentials"
+echo "    db: wordpress"
+echo "  user: wordpress"
+echo "  pass: $WP_DB_PASS"
+echo 
+
 sudo reboot
--- setup_web.sh
+++ setup_web.sh
@@ -21,17 +21,58 @@ sudo a2enmod deflate
 sudo mkdir -p /var/www
 
 # install mysql
+# http://gercogandia.blogspot.ru/2012/11/automatic-unattended-install-of.html
 sudo echo "mysql-server mysql-server/root_password password $MYSQL_ADMIN_PASSWORD" | sudo debconf-set-selections
 sudo echo "mysql-server mysql-server/root_password_again password $MYSQL_ADMIN_PASSWORD" | sudo debconf-set-selections
 sudo apt-get -q -y install mysql-server mysql-client php5-mysql libapache2-mod-auth-mysql
 
+# create database
+mysqladmin --user=root --password=$MYSQL_ADMIN_PASSWORD create wordpress
+
+# create user & database
+WP_DB_PASS=`cat /dev/urandom | tr -dc A-Za-z0-9 | head -c 10`
+export $WP_DB_PASS;
+echo "create database wordpress;" > /tmp/database.sql
+echo "create user 'wordpress'@'localhost' identified by '$WP_DB_PASS';" >> /tmp/database.sql
+echo "grant all privileges on wordpress.* to 'wordpress'@'localhost';" >> /tmp/database.sql
+echo "flush privileges;" >> /tmp/database.sql
+mysql --user=root --password=$MYSQL_ADMIN_PASSWORD < /tmp/database.sql
+
 # install wordpress
+cd /tmp
+wget http://wordpress.org/latest.tar.gz
+tar -xfvz latest.tar.gz
+sudo mv wordpress /var/www
 
+echo "<VirtualHost *:80>"                 > /tmp/wordpress.apache
+echo "  ServerName $HOSTNAME"            >> /tmp/wordpress.apache
+echo "  DocumentRoot /var/www/wordpress" >> /tmp/wordpress.apache
+echo "  DirectoryIndex index.php"        >> /tmp/wordpress.apache
+echo "  <Directory /var/www/wordpress/>" >> /tmp/wordpress.apache
+echo "    AllowOverride All"             >> /tmp/wordpress.apache
+echo "    Order Deny, Allow"             >> /tmp/wordpress.apache
+echo "    Allow from all"                >> /tmp/wordpress.apache
+echo "  </Directory>"                    >> /tmp/wordpress.apache
+echo "</VirtualHost>"                    >> /tmp/wordpress.apache
 
-# chown
+sudo mv /tmp/wordpress.apache /etc/apache2/sites-available/wordpress
+sudo a2ensite wordpress
+sudo service apache2 restart
+
+# configure wordpress
+sudo cd /var/www
+sudo chmod -R 777 wordpress
+
+# create script for change permissions
+#echo "sudo find /var/www/wordpress -type d -exec chmod 755 {} \;"  > /tmp/change.permissions.sh
+#echo "sudo find /var/www/wordpress -type f -exec chmod 644 {} \;" >> /tmp/change.permissions.sh
+#sudo mv /tmp/change.permissions.sh 
+
+# make test files
 echo "<?php phpinfo()?>" > /tmp/test.php
 echo "<html><body><h2>It seems to be working!</h2></body></html>" > /tmp/test.html
 sudo mv /tmp/test.php  /var/www
 sudo mv /tmp/test.html /var/www
 
+# chown
 sudo chown -R www-data:www-data /var/www
